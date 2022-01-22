b38792436bc14ff866f27299ee47c362fee35020
Thorsten Lorenz <thlorenz@gmx.de>
2022-01-22 16:02:22 -0600

    chore: setup gumdrop and tests

--- gumdrop/js/src/mpl-gumdrop.ts
+++ gumdrop/js/src/mpl-gumdrop.ts
@@ -1,1 +1,10 @@
-export const GUMDROP = 'Will expose SDK here';
+import { PublicKey } from '@solana/web3.js';
+
+export * from './generated/accounts';
+export * from './generated/errors';
+export * from './generated/instructions';
+export * from './generated/types';
+
+export const PROGRAM_ID_VAL = 'gdrpGjVffourzkdDRrQmySw4aTHr8a3xmQzzxSwFD1a';
+export const PROGRAM_ID = new PublicKey(PROGRAM_ID_VAL);
+
